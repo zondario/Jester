e047c748aa11a3eadbba52d6f43002588dc34d58
zondario <ivo.cenov1@gmail.com>
2017-04-20 12:47:31 +0300

    Order bugs fixed

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -21,33 +21,63 @@ use Symfony\Component\Validator\Constraints\Date;
 class OrderController extends Controller
 {
      const ADDED_STATUS = 1;
+     const DEFAULT_QUANTITY= 1;
     /**
-     * @Route("/order/{id}/{quantity}", name="orderProduct")
+     * @Route("/order/{id}", name="orderProduct")
      */
-    public function orderProduct($id,$quantity)
+    public function orderProduct($id)
     {
-        /** @var User $currentUser */
-        $currentUser= $this->getUser();
-        /** @var StockRepository $stockRepo */
-        $em = $this->getDoctrine()->getManager();
-        $status = $em->getRepository(Status::class)->findOneBy(["id"=>self::ADDED_STATUS]);
+       /** @var User $currentUser */
+       $currentUser= $this->getUser();
+       /** @var StockRepository $stockRepo */
+       $em = $this->getDoctrine()->getManager();
+       $status = $em->getRepository(Status::class)->findOneBy(["id"=>self::ADDED_STATUS]);
        $stock = $em->getRepository(Stock::class)->findOneBy(['id'=>$id]);
-       $order = new ProductOrder();
-       $order->setUser($currentUser);
-       $order->setStock($stock);
-       $order->setStatus($status);
-       $order->setQuantity($quantity);
-       $calcPrice = $order->getStock()->getProduct()->getPrice();
-       if($stock->getPromotions()->count()>0){
-           /** @var Promotion[]|ArrayCollection $promotions */
-           $maxPromotion = $this->get("app.promotion")->findMaxPromotionForStock($stock);
-           $calcPrice = $calcPrice - ($calcPrice * ($maxPromotion->getPercentage()/100));
+       $userOrderOfCurrStock = null;
+        foreach ($currentUser->getOrders() as $order) {
+            if($stock===$order->getStock()&&$order->getStatus()->getId()===self::ADDED_STATUS){
+                $userOrderOfCurrStock = $order;
+            }
        }
-       $order->setCalculatedSinglePrice($calcPrice);
-       $order->setFinalPrice($order->getQuantity()*$order->getCalculatedSinglePrice());
-       $order->setAddedOn(new \DateTime());
-       $em->persist($order);
-       $em->flush();
+       if(!$userOrderOfCurrStock)
+       {
+           $order = new ProductOrder();
+           $order->setUser($currentUser);
+           $order->setStock($stock);
+           $order->setStatus($status);
+           $order->setQuantity(self::DEFAULT_QUANTITY);
+           $calcPrice = $order->getStock()->getProduct()->getPrice();
+           $maxPromotion = $this->get("app.promotion")->findMaxPromotionForStock($stock);
+           if($maxPromotion){
+               $calcPrice = $calcPrice - ($calcPrice * ($maxPromotion->getPercentage()/100));
+           }
+           $order->setCalculatedSinglePrice($calcPrice);
+           $order->setFinalPrice($order->getQuantity()*$order->getCalculatedSinglePrice());
+           $order->setAddedOn(new \DateTime());
+           $em->persist($order);
+
+
+       }else{
+            $now = new \DateTime();
+           $order = $userOrderOfCurrStock;
+           $maxPromotion = $this->get("app.promotion")->findEffectivePromotionForOrder($order);
+           if($maxPromotion->getStartsOn()<=$now && $maxPromotion->getEndsOn() >= $now) {
+               $order->setQuantity($order->getQuantity() + self::DEFAULT_QUANTITY);
+               $calcPrice = $order->getStock()->getProduct()->getPrice();
+
+
+               if ($maxPromotion) {
+                   $calcPrice = $calcPrice - ($calcPrice * ($maxPromotion->getPercentage() / 100));
+               }
+               $order->setCalculatedSinglePrice($calcPrice);
+               $order->setFinalPrice($order->getQuantity() * $order->getCalculatedSinglePrice());
+           }else
+           {
+               $this->addFlash("danger","Cannot complete your order of ".$order->getStock()->getProduct()->getName()." because there are differences between prices. Please delete or confirm your order in the cart to be able to add this stock");
+           }
+
+       }
+        $em->flush();
        return $this->redirectToRoute("detailsView",["id"=>$id]);
     }
     /**
